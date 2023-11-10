e896a4deab6c7b59af379496b13478da88666361
LucyFireID <mynameisghofur@gmail.com>
2023-11-11 00:57:21 +0700

    navbar

--- resources/views/navbar.php
+++ resources/views/navbar.php
@@ -1,8 +1,8 @@
 <nav class="navbar fixed-top navbar-expand-lg bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="">
-            <img src="public/img/peteripung-logo.png" alt="" width="50">
-            <span><b>Peter Ipung</b></span>
+            <img src="public/img/peteripung-logo.png" alt="" width="30">
+            <span class="align-middle fs-3"><b>Peter Ipung</b></span>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
